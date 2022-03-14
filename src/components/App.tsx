fb6477afb6c50bf84fe21542ac4e6d1244561b58
Alexander Bezrukov <alex.bezroukov@gmail.com>
2022-03-15 01:32:33 +0300

    Исправления для eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { ReactElement } from "react";
+
 import { Todo } from "./Todo";
 import { Counter } from "./Counter";
 import styled from "styled-components";
@@ -8,7 +10,7 @@ const Div = styled.div`
 `;
 
 class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
+  public render(): ReactElement {
     return (
       <Div>
         <Todo />
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -2,12 +2,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import * as TestUtils from "react-dom/test-utils";
 import App from "../src/components/App";
+import { ReactElement } from "react";
 
 it("App is rendered", () => {
   // Render App in the document
-  // @ts-ignore
-  const appElement: App = TestUtils.renderIntoDocument(<App />);
+  const appElement = TestUtils.renderIntoDocument(
+    (<App />) as ReactElement,
+  ) as App;
 
+  // Just for tests
+  // eslint-disable-next-line react/no-find-dom-node
   const appNode = ReactDOM.findDOMNode(appElement);
 
   // Verify text content
